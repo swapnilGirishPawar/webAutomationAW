9ee5a38fbbe6809fd565832e3eea3fd4a3ae8f05
swapnilGirishPawar <swapnil.girish@anywhere.co>
2023-07-03 14:25:28 +0530

    added Data Faker class functions

--- src/test/java/CommonUtils/DataFaker.java
+++ src/test/java/CommonUtils/DataFaker.java
@@ -2,15 +2,28 @@ package CommonUtils;
 
 import net.datafaker.Faker;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.jetbrains.annotations.NotNull;
+
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.commons.lang3.RandomUtils.nextLong;
 
 public class DataFaker {
     public static Faker faker = new Faker();
+
+    // facker functions list:-
+//        1. getRandomNumber
+//        2. getRandomAlphabet
+//        3. getRandomNumber
+//        4. getRandoMobileNumber
+//        5. getRandomEmailAdderess
+//        6. getRandomDate
     public static String getRandomNumber(int count){
         return faker.number().digits(count);
     }
 
-    public static @NotNull String getRandomAlphabate(int size){
+    public static String getRandomAlphabet(int size){
         return RandomStringUtils.randomAlphabetic(size);
     }
 
@@ -20,5 +33,12 @@ public class DataFaker {
 
     public static String getRandoMobileNumber(){
          return faker.number().numberBetween(6, 9) + faker.number().digits(9);
+    }
+    public static String getRandomEmailAdderess(){
+        return faker.internet().emailAddress();
+    }
+
+    public static String getRandomDate(){
+        return faker.date().birthday().toString().substring(0,10);
     }
 }
